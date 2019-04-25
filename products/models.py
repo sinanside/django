f50c4a6362a7e5274de99fa7946f5962b4726eda
sinanside <31740631+sinanside@users.noreply.github.com>
2019-04-26 00:02:17 +0300

    Sayfaları güncelleyelim

--- products/models.py
+++ products/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
-    price = models.DecimalField(DecimalField=2, max_digits=20, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20, null=True)
