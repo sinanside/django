024ad82b1ab27ae5914efd3f30f4d2f9f17a2de8
sinanside <31740631+sinanside@users.noreply.github.com>
2019-04-25 13:52:42 +0300

    Sayfaları güncelleyelim

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,13 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField()
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Adınızı Giriniz'
+            }
+        )
+    )
     email = forms.EmailField()
     content = forms.CharField()
