11fa53d1d1c199bdd20655b4a62cc8983e6d39ab
sinanside <31740631+sinanside@users.noreply.github.com>
2019-04-26 00:19:31 +0300

    Sayfaları güncelleyelim

--- products/models.py
+++ products/models.py
@@ -7,3 +7,9 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
