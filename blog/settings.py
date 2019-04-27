3885f11a8f8a72663bb2434288ae571d3e4a0f15
sinanside <31740631+sinanside@users.noreply.github.com>
2019-04-28 00:52:35 +0300

    Sayfaları güncelleyelim

--- blog/settings.py
+++ blog/settings.py
@@ -86,8 +86,9 @@ DATABASES = {
         'PASSWORD': 'pythonmysql2019',
         'HOST': 'dizaynschool.mysql.pythonanywhere-services.com',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
