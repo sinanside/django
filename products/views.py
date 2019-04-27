9c8a1dcc3ad2bb8c3c407bb25311b23312a91c59
sinanside <31740631+sinanside@users.noreply.github.com>
2019-04-27 11:06:50 +0300

    Sayfaları güncelleyelim

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from .models import Product
@@ -19,3 +19,20 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    # def get_context_data(self, **kwargs):
+    #    data = super(ProductListView, self).get_context_data(**kwargs)
+    #    return data
+
+
+def product_detail_view(request, pk=None):
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
