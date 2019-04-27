00bf4da3fe5e04581cc8a173cf05eed19837adf1
sinanside <31740631+sinanside@users.noreply.github.com>
2019-04-27 20:59:58 +0300

    Sayfaları güncelleyelim

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ def upload_image_path(instance, filename):
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, null=True)
     image = models.ImageField(verbose_name='Resim', upload_to=upload_image_path, null=True, blank=True)
